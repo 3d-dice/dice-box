851bba7cf89516de8bcbb880f599fba099315412
frankieali <frankieali4@gmail.com>
2021-09-26 11:46:43 -0400

    doesn't like arrays as paths

--- copyAssets.js
+++ copyAssets.js
@@ -3,7 +3,7 @@
 const copydir = require('copy-dir');
 const path = require('path');
 
-const filesToCopy = ['./dist/assets']
+const filesToCopy = './dist/assets'
 
 // User's local directory
 const userPath = path.join(process.env.INIT_CWD,'/public/assets')

dbd51d095596baff3655e904898415e940701666
frankieali <frankieali4@gmail.com>
2021-09-26 11:41:51 -0400

    fix copy-dir bug with undefined options

--- copyAssets.js
+++ copyAssets.js
@@ -6,7 +6,11 @@ const path = require('path');
 const filesToCopy = ['./dist/assets']
 
 // User's local directory
-const userPath = path.join(process.env.INIT_CWD,'/public/assets')
+const userPath = path.join(process.env.INIT_CWD,'/public/assets', {
+  utimes: true,  // keep add time and modify time
+  mode: true,    // keep file mode
+  cover: true    // cover file when exists, default is true
+})
 
 // Moving files to user's local directory
 copydir(filesToCopy, userPath)

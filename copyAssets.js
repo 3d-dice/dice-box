ad2855b05f9f4758b39d1a27dadad571a4cf2717
frankieali <frankieali4@gmail.com>
2021-12-06 13:54:30 -0500

    attempt to fix the install

--- copyAssets.js
+++ copyAssets.js
@@ -2,11 +2,17 @@
 
 const copydir = require('copy-dir');
 const path = require('path');
+const fs = require('fs');
 
 const filesToCopy = './dist/assets'
 
 // User's local directory
 const userPath = path.join(process.env.INIT_CWD,'/public/assets')
+
+// Creates directory if it doesn't exist
+fs.mkdir(userPath, { recursive: true }, (err) => {
+  if (err) throw err;
+});
 
 // Moving files to user's local directory
 copydir(filesToCopy, userPath, {

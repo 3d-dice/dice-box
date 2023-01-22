d75aacb4d80c48bf53d97c271cd283688d5a195c
frankieali <frankieali4@gmail.com>
2023-01-22 10:39:27 -0500

    update build to create min.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const { defineConfig } = require('vite')
 const copy = require('rollup-plugin-copy')
+const minifyEs = require('./rollup-plugin-minifyEs').default
 // const { visualizer } = require('rollup-plugin-visualizer');
 
 module.exports = defineConfig({
@@ -9,7 +10,11 @@ module.exports = defineConfig({
     lib: {
       entry: path.resolve(__dirname, 'src/index.js'),
       name: 'dice-box',
-      fileName: (format) => `dice-box.${format}.js`
+			// format: ['es','esm'],
+			fileName: (format) => ({
+        es: `dice-box.es.js`,
+        esm: `dice-box.es.min.js`,
+      })[format]
     },
 		assetsDir: 'assets/dice-box',
     rollupOptions: {
@@ -17,16 +22,21 @@ module.exports = defineConfig({
       input: {
 				main: path.resolve(__dirname, 'src/index.js')
 			},
-			output: [{
-				format: 'es',
-				manualChunks: {
-					// babylon: ['@babylonjs/core','@babylonjs/loaders','@babylonjs/materials']
+			output: [
+				{
+					format: "es",
+					chunkFileNames: (chunkInfo) => `${chunkInfo.name}.js`,
+					sourcemap: true,
 				},
-				entryFileNames: 'assets/dice-box.es.js',
-				chunkFileNames: 'assets/[name].js',
-				assetFileNames: 'assets/[name].[ext]',
-				sourcemap: false,
-			}],
+				{
+					format: "esm",
+					chunkFileNames: (chunkInfo) => `${chunkInfo.name}.min.js`,
+					sourcemap: false,
+					plugins: [
+						minifyEs(),
+					]
+				}
+			],
 			plugins: [
 				copy({
 					targets: [

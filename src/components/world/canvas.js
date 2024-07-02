748a4aa16d27ae2813a896403030d14f7b485489
frankieali <frankieali4@gmail.com>
2024-07-02 08:55:07 -0400

    Allow canvas to be appended to document body without DOM container

--- src/components/world/canvas.js
+++ src/components/world/canvas.js
@@ -1,25 +1,24 @@
 function createCanvas(options) {
   const { selector, id } = options
 
-  if (!selector || typeof selector !== 'string') {
-    throw(new Error("You must provide a DOM selector as the first argument in order to render the Dice Box"))
+  let container = document.body
+  let canvas = document.createElement('canvas')
+  canvas.id = id
+
+  if(selector) {
+    if (typeof selector !== 'string') {
+      throw(new Error("You must provide a DOM selector as the first argument in order to render the Dice Box"))
+    }
+
+    container = document.querySelector(selector)
+
+    if(!container?.nodeName){
+      throw(new Error(`DiceBox target DOM node: '${selector}' not found or not available yet. Try invoking inside a DOMContentLoaded event`))
+    }
   }
 
-  let canvas
-  const container = document.querySelector(selector)
+  container.appendChild(canvas)
 
-  if(!container?.nodeName){
-    throw(new Error(`DiceBox target DOM node: '${selector}' not found or not available yet. Try invoking inside a DOMContentLoaded event`))
-  }
-  
-  if(container.nodeName.toLowerCase() !== 'canvas') {
-    canvas = document.createElement('canvas')
-    canvas.id = id
-    container.appendChild(canvas)
-  } 
-  else {
-    canvas = container
-  }
   return canvas
 }
 

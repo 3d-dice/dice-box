8ee1c3d9735f61431fd8c005770d0180225731f4
frankieali <frankieali4@gmail.com>
2024-07-08 12:03:18 -0400

    remove debugger

--- src/components/world/engine.js
+++ src/components/world/engine.js
@@ -1,5 +1,4 @@
 import { Engine } from '@babylonjs/core/Engines/engine'
-import "@babylonjs/core/Engines/Extensions/engine.debugging"
 
 function createEngine(canvas) {
   const engine = new Engine(canvas, true, {
